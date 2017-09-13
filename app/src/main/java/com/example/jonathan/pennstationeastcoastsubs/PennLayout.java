ac81d03e97941fcc4c248b486e01a0702c2c8b7c
jrbrowning1 <31349530+jrbrowning1@users.noreply.github.com>
2017-09-13 10:01:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jonathan/pennstationeastcoastsubs/PennLayout.java
@@ -0,0 +1,13 @@
+package com.example.jonathan.pennstationeastcoastsubs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PennLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penn_layout);
+    }
+}
